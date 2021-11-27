7d0d2efc49850d01d1ee93df67f0ea30dafcd6a0
Furkan Konyali <furkan.konyali@kobil.com>
2021-11-27 15:12:48 +0300

    header component update style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,23 +6,23 @@ import './style.scss';
 function Header () {
   return (
     <header className="header">
-        <div className="header__container">
-            <h1 className="header__title header--maxwidth">
-              Lorem Ipsum <br/>
-              Dolor sit amet consectetur..
-            </h1>
-            <div className="header__desc header--maxwidth">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. 
-              </p>
-            </div>
-            <div className="header__img">
-              <img alt="Movie" src={headerImg} />
-            </div>
-            <Button>
-              CTA BUTTON
-            </Button>
+      <div className="header__container">
+        <h1 className="header__title header--maxwidth">
+          Lorem Ipsum <br/>
+          Dolor sit amet consectetur..
+        </h1>
+        <div className="header__desc header--maxwidth">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor. 
+          </p>
         </div>
+        <div className="header__img">
+          <img alt="Movie" src={headerImg} />
+        </div>
+        <Button>
+          CTA BUTTON
+        </Button>
+      </div>
     </header>
   )
 }
